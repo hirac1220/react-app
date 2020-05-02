4d0f27d8a87510a88635b442b3cd3c5ec02c3b8f
hriac <kimi1220@gmail.com>
2020-05-03 02:43:48 +0900

    Add Router/Link

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import Booklist from './components/Booklist';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 function App() {
   const languages = ['React', 'Vue', 'Angular'];
   return (
-    <div className="App">
-      <h1>react app</h1>
-      <Booklist
-        language={languages[0]}
-      />
-      <Booklist
-        language={languages[1]}
-      />
-      <Booklist
-        language={languages[2]}
-      />
-    </div>
+    <BrowserRouter>
+      <div>
+        <h1>react app</h1>
+        <ul>
+          <li><Link to='/'>React</Link></li>
+          <li><Link to='/vue'>Vue</Link></li>
+          <li><Link to='/angular'>Angular</Link></li>
+        </ul>
+        <hr />
+        <Route exact path='/' render={props => <Booklist language={languages[0]} />}/>
+        <Route path='/vue' render={props => <Booklist language={languages[1]}/>}/>
+        <Route path='/angular' render={props => <Booklist language={languages[2]}/>} />
+      </div>
+    </BrowserRouter>
   );
 }
 
